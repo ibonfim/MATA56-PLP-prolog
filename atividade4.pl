b59a53e04d66b847a5e32e65571720210bf62675
ibonfim <indaiara.bonfim@jusbrasil.com.br>
2025-01-28 18:39:09 -0300

    subindo v1 com verificacao de conflito de horario

--- atividade4.pl
+++ atividade4.pl
@@ -1,4 +1,4 @@
-:- dynamic alocada/4.  % Agora aloca a sala, o datashow, o horário de início e fim
+:- dynamic alocada/4.  % aloca a sala, o datashow, o horário de início e fim
 :- dynamic disponivel/1.
 
 % Definindo os datashows e salas = constantes nomeadas
@@ -17,12 +17,24 @@ disponivel(d3).
 
 % Regras para alocar datashow com horários
 alocar_datashow(Sala, Datashow, Inicio, Fim) :-
-   
+    sala(Sala),
+    datashow(Datashow),
+    \+ alocada(Sala, _, _, _),              % Verifica se a sala não tem datashow alocado
+    \+ conflito(Datashow, Inicio, Fim),     % Verifica se o datashow está disponível no horário
+    assert(alocada(Sala, Datashow, Inicio, Fim)),  % Realiza a alocação com os horários
+    retract(disponivel(Datashow)),                % Marca o datashow como não disponível
+    format('Datashow ~w alocado para a sala ~w de ~w até ~w.~n', [Datashow, Sala, Inicio, Fim]).
 
 % Verifica se há conflito de horários com o datashow em qualquer sala
 conflito(Datashow, Inicio, Fim) :-
-  
+    alocada(_, Datashow, InicioExistente, FimExistente),
+    (Inicio < FimExistente, Fim > InicioExistente), 
+    % Confirma que os horários se sobrepõem
+    format('Não é possível alocar o datashow ~w devido a conflito de horário.~n', [Datashow]).
 
 % Regras para liberar datashow
 desalocar_datashow(Sala, Datashow) :-
-   
+    alocada(Sala, Datashow, _, _),           % Verifica se o datashow está alocado na sala
+    retract(alocada(Sala, Datashow, _, _)),  % Remove a alocação
+    assert(disponivel(Datashow)),            % Marca o datashow como disponível
+    format('Datashow ~w liberado da sala ~w.~n', [Datashow, Sala]).
